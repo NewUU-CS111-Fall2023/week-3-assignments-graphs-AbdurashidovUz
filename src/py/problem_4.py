df34d0e69de69092a92e7988af308b4f7871a4a8
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2023-10-31 21:29:43 +0500

    Update problem_4.py

--- src/py/problem_4.py
+++ src/py/problem_4.py
@@ -1,1 +1,8 @@
+n = int(input())  # Number of genome fragments
+fragments = [input() for _ in range(n)]
+
+fragments = [fragment for fragment in fragments if not fragment.isdigit()]
+fragment_characters = [[char for char in fragment] for fragment in fragments] // chars
+
+print (fragment_characters)
 
